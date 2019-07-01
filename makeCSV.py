84203dafee75ccbe3704a82de7481d36cf514dad
jmalish <jmalish@users.noreply.github.com>
2019-07-01 14:56:05 -0400

    Add receipt style numbering option

--- makeCSV.py
+++ makeCSV.py
@@ -1,11 +1,11 @@
-# imports
+# imports wDp85suUCh46GZ6
 import csv
 import sys
 import math
 
 # make sure we're getting the right number of arguments
 if (len(sys.argv) < 5):
-    print '<n-up> <needed> <startNum> <leading zeros>'
+    print '<n-up> <needed> <startNum> <leading zeros> <?receipt style>'
     quit()
 
 # variables
@@ -15,6 +15,11 @@ nUp = int(sys.argv[1])  # how many files we have up on the page
 needed = float(sys.argv[2])  # how many things we're printing
 startNum = int(sys.argv[3])  # what number to start at
 leadingZeros = int(sys.argv[4]) + 1  # how many leading zeros
+
+receiptStyle = False  # whether or not we do receipt style numbering
+if (len(sys.argv) > 5):
+    if (sys.argv[5].lower() == "true"):
+        receiptStyle = True
 
 rowsNeeded = int(math.ceil(needed/nUp))  # calculate how many rows we need
 
@@ -27,16 +32,30 @@ for i in range(0, nUp):
 csvData.append(headers)  # add headers to csv data
 
 # build numbers array
-for i in range(0, rowsNeeded):
-    row = []
-    for j in range(0, nUp):
-        row.append(str((1 + i + (rowsNeeded * j) + startNum))
-                   .zfill(leadingZeros))  # add the leading zeros if necessary
-    csvData.append(row)
+if (not receiptStyle):
+    for i in range(0, rowsNeeded):
+        row = []
+        for j in range(0, nUp):
+            row.append(str((i + (rowsNeeded * j) + startNum))
+                       .zfill(leadingZeros))  # add the leading zeros
+        csvData.append(row)
+else:  # receipt style numbering
+    for i in range(0, rowsNeeded):
+        row = []
+        for j in range(0, nUp):
+            row.append(str(j + startNum + (i * nUp))
+                       .zfill(leadingZeros))  # leading zeros
+        csvData.append(row)
+
 
 # build file name, split up to avoid the "lines too long" thing
 fileName = str(nUp) + "up " + str(startNum) + "-"
-fileName = fileName + csvData[len(csvData) - 1][nUp - 1] + ".csv"
+fileName = fileName + str(csvData[len(csvData) - 1][nUp - 1])
+
+if (receiptStyle):
+    fileName = fileName + " receiptStyle"
+
+fileName = fileName + ".csv"
 
 # create and write to file
 with open(fileName, 'wb') as csvFile:
